f7253c50beec73c833b36fd3d38fc0743b2ae1d2
Raul Pacho <raulpachovivas@gmail.com>
2020-11-09 16:03:06 +0100

    programa con photoshop

---
+++ SP_Tema4_Ejer4/Horse.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SP_Tema4_Ejer4
+{
+    class Horse
+    {
+        private static object l = new object();
+        public static bool goal = false;
+        private int distance = 10;
+        public string HorseName { get; set; }
+        public int horseId;
+        private int posTop;
+        private string space = "";
+        private string face = "";
+        Random rand = new Random();
+        public static int IdWinner { get; set; }
+
+        public /*object*/ void Correr()
+        {
+            string aux = "";
+            while (!goal)
+            {
+                if (!goal)
+                {
+                    distance--;
+                    aux += space.Substring(0, space.Length - distance);
+                    space = space.Substring(0, distance);
+                    lock (l)
+                    {
+
+                        Console.SetCursorPosition(1, posTop);
+                        if (distance == 1)
+                        {
+                            Console.WriteLine("{2}{0}{3}{1}", " ", "|", aux, space);
+                            Console.SetCursorPosition(51, posTop);
+                            Console.WriteLine("{0}{1}", " |", face);
+                            Console.SetCursorPosition(1, 27);
+                            Console.WriteLine("Enter key to continue...");
+                            goal = true;
+                            IdWinner = horseId;
+                        }
+                        else
+                        {
+                            Console.WriteLine("{2}{0}{3}{1}", face, "|", aux, space);
+                        }
+                    }
+                    /*if(distance == 1)
+                    {
+                        Console.SetCursorPosition(51, posTop);
+                        Console.WriteLine("{0}{1}", " |", "h");
+                        goal = true;
+                    }*/
+                    Thread.Sleep(100 * rand.Next(2, 6));
+                }
+
+            }
+            //return null;
+        }
+
+        public Horse(int id, string name, int posTop, string face)
+        {
+            horseId = id;
+            HorseName = name;
+            this.posTop = posTop;
+            this.face = face;
+            goal = false;
+            for (int i = 0; i < distance; i++)
+            {
+                space += " ";
+            }
+        }
+
+
+    }
+}
---
+++ SP_Tema4_Ejer4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SP_Tema4_Ejer4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fuckedUp = true;
+            string repeat = "1";
+            int bet = -1;
+
+
+
+            while (repeat == "1")
+            {
+                List<Thread> threads = new List<Thread>();
+                List<Horse> horses = new List<Horse>();
+
+                horses.Add(new Horse(0, "HorseMaria", 1, "1"));
+                horses.Add(new Horse(1, "HorseLuis", 3, "2"));
+                horses.Add(new Horse(2, "MariaHorse", 5, "3"));
+                horses.Add(new Horse(3, "Ramon", 7, "4"));
+                horses.Add(new Horse(4, "HorseCarlos", 9, "5"));
+                fuckedUp = true;
+                while (fuckedUp)
+                {
+                    fuckedUp = false;
+                    Console.WriteLine("Introduce your bet modafaca");
+                    Console.WriteLine("1. HorseMaria");
+                    Console.WriteLine("2. HorseLuis");
+                    Console.WriteLine("3. MariaHorse");
+                    Console.WriteLine("4. Ramon");
+                    Console.WriteLine("5. HorseCarlos");
+
+                    if (!int.TryParse(Console.ReadLine(), out bet))
+                    {
+
+                        Console.WriteLine("Thats not a number, you moron");
+                        fuckedUp = true;
+                    }
+                    else
+                    {
+                        bet--;
+                        if (bet >= horses.Count || bet < 0)
+                        {
+                            Console.WriteLine("Thats not a horse, you moron");
+                            fuckedUp = true;
+                        }
+                    }
+                }
+                Console.Clear();
+                for (int i = 0; i < horses.Count; i++)
+                {
+                    threads.Add(new Thread(horses[i].Correr));
+                    threads[i].Start();
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("And the winner is: " + horses[Horse.IdWinner].HorseName);
+                string str = bet == Horse.IdWinner ? "And yout win, feeling right?" : "Hah! you lost";
+                Console.WriteLine(str);
+
+
+                Console.WriteLine("Introduce 1 to repeat Enter to exit");
+                repeat = Console.ReadLine();
+
+            }
+        }
+    }
+}
